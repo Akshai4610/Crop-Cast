99d9b49ced3f8519c683ce79ae13fec4882626ab
Akshai4610 <akshai4610@gmail.com>
2026-02-11 01:44:09 +0530

    clean repo using updated .gitignore

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -10,7 +10,7 @@ import axios from "axios";
 // NEVER add /api again in routes
 // ======================================================
 const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
